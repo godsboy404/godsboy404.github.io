308838f78df9e0e0e9d1359e772c6fd9ad05b11a
润泽 <zrz515@outlook.com>
2025-09-15 13:56:24 +0000

    final config

--- .vitepress/theme/assets/themeConfig.mjs
+++ .vitepress/theme/assets/themeConfig.mjs
@@ -323,7 +323,7 @@ export const themeConfig = {
   // 音乐播放器
   // https://github.com/imsyy/Meting-API
   music: {
-    enable: true,
+    enable: false,
     // url
     url: "https://meting-api-omega.vercel.app",
     // id (9379831714)
